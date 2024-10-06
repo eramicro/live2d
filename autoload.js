2f6c3d4d2298594ecab0f4d2e3f8ec5507060c5a
eramicro <645452175@qq.com>
2024-10-06 22:24:37 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -3,7 +3,6 @@ const live2d_path = "https://live2d.icucloud.vip/widget/";
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
 		let tag;
-
 		if (type === "css") {
 			tag = document.createElement("link");
 			tag.rel = "stylesheet";
@@ -20,7 +19,6 @@ function loadExternalResource(url, type) {
 		}
 	});
 }
-
 if (screen.width >= 768) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
